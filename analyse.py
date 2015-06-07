b2b812f340f3268c141f0ef580ed0659b6b83b92
Hrom <hrom@localhost.localdomain>
2015-06-07 23:08:10 +0200

    last final

--- analyse.py
+++ analyse.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 dic={}
-s=[]
-s1=[]
 
 import sys
 
 for filename in sys.argv[1:]:
     dic[filename]={"task-clock":[],"seconds time elapsed":[]}    
-    for line in open(filename):
+    f=open(filename)
+    for line in f:
         if 'task-clock' in line:
             line = line.split()
-            s.append(float(line [0]))
             dic[filename]["task-clock"].append(float(line [0]))
         if 'seconds time elapsed' in line:
             line = line.split()
-            s1.append(float(line [0]))
             dic[filename]["seconds time elapsed"].append(float(line [0]))
+    f.close()                    
 for filename in sorted(dic.keys()):
     print "%20s: %20s %20s" % (filename,', '.join(map(str,(dic[filename]["task-clock"]))),', '.join(map(str,(dic[filename]["seconds time elapsed"]))))
-print "%20s: %20s %20s" % ('Average', sum(s)/len(s),sum(s1)/len(s1))
-        
 
+def averagedic(x):
+    a=[z[x][y] for z in dic.values() for y in range(len(z[x]))]
+    return sum(a)/len(a)
+
+print "%20s: %20s %20s" % ('Average',averagedic('task-clock'),averagedic('task-clock'))
