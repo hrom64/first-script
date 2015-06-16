602bc75f38812acc693a8f14de4c6e30ca7ba0a7
Hrom <hrom@localhost.localdomain>
2015-06-16 22:44:59 +0200

    not working printout

--- analyse.py
+++ analyse.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python
-dic={}
 
 import sys,re
 
-for filename in sys.argv[1:]:
-    dic[filename]={"task-clock":[],"seconds time elapsed":[]}    
-    f=open(filename)
-    for line in f:
-        m = re.search('(\d+[.]?\d*)\s+task-clock', line)
-        if m:
-            dic[filename]["task-clock"].append(float(m.group(1)))
-  
-        m = re.search('(\d+[.]?\d*)\s+seconds time elapsed', line)
-        if m :
-            dic[filename]["seconds time elapsed"].append(float(m.group(1)))
-    f.close()                    
-for filename in sorted(dic.keys()):
-    print "%20s: %20s %20s" % (filename,', '.join(map(str,(dic[filename]["task-clock"]))),', '.join(map(str,(dic[filename]["seconds time elapsed"]))))
+class analyser:
+    def __init__(self,desk):
+        self.d=desk
+        self.dic={}
+        self.str_name=[sh[0] for sh in self.d]
+    def parse(self,x):        
+        for filename in x:  
+            f=open(filename)
+            for line in f:
+                for sh in self.d:
+                    name=sh[0]
+                    string=sh[1]
+                    m = re.search(string, line)
+                    self.dic.setdefault(filename, {}).setdefault(name, [])
+                    if m:
+                        self.dic[filename][name].append(float(m.group(1)))
+            f.close()
+    def average(self):
+        a=[z[y] for z in self.dic.values() for y in range(len(z))]
+        return sum(a)/len(a)    
+    def column(self):
+        a=[]
+        for x in self.str_name:
+            a.append("', '.join(map(str,(self.dic[filename][%s]))))" % (x)) #for x in self.str_name)] 
+        return ', '.join(a) 
+    def prn(self,filename):
+        return ', '.join(map(str,(self.dic[filename].setdefault(filename,[]))))
+    def sss(self):
+        for filename in sorted(self.dic.keys()):
+            return "%20s: %20s " % (filename,', '.join(map(str,(self.dic[filename]["task-clock"]))))
 
-def averagedic(x):
-    a=[z[x][y] for z in dic.values() for y in range(len(z[x]))]
-    return sum(a)/len(a)
 
-print "%20s: %20s %20s" % ('Average',averagedic('task-clock'),averagedic('task-clock'))
+data=analyser(   [ ("task-clock",'(\d+[.]?\d*)\s+task-clock'), ("seconds time elapsed",'(\d+[.]?\d*)\s+seconds time elapsed') ]   )        
+#task=analyser("task-clock",'(\d+[.]?\d*)\s+task-clock')
+#seconds=analyser("seconds time elapsed",'(\d+[.]?\d*)\s+seconds time elapsed')
+
+
+#for filename in sys.argv[1:]:  
+#    f=open(filename)
+#    for line in f:
+#        task.search(filename,line)
+#        seconds.search(filename,line)
+
+#    f.close()                    
+#for filename in sorted(task.dic.keys()):
+#    print "%20s: %20s %20s" % (filename,task.prn(filename),seconds.prn(filename))
+
+#print "%20s: %20s %20s" % ('Average',task.average(),seconds.average())
+
+data.parse (  sys.argv[1:])
+print data.sss()
+
+
